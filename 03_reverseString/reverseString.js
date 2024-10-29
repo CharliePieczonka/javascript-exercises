95738ab31fa137dbaf7089e82bb89e1503d0e5d8
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2024-10-29 18:50:03 +0000

    Complete JavaScript reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse = '';
+    let stringLength = string.length;
 
+    for (let i = string.length; i > 0; i--) {
+        reverse += string.substring(i-1, i);
+    }
+
+    return reverse;
 };
 
 // Do not edit below this line
