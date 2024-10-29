391ebefc96c201977d424c4a05d039585ae380d2
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2024-10-29 18:35:28 +0000

    Complete JavaScript repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    
+    if(repeats < 0){
+        return 'ERROR';
+    }
+    
+    repeated = '';
+    for(let i = 0; i < repeats; i++) {
+        repeated += string;
+    }
 
+    return repeated;
 };
 
 // Do not edit below this line
