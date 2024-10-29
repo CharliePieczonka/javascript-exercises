acec62c45e7048047d6d5868d012efb41a7efc88
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2024-10-29 19:41:46 +0000

    Complete JavaScript removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let filteredArray = array;
+    numRemove = arguments.length - 1;
+
+    for(let i = 1; i <= numRemove; i++){
+        filteredArray = filteredArray.filter(x => x !== arguments[i]);
+    }
+    
+    return filteredArray;
 };
 
 // Do not edit below this line
