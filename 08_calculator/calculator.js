51edc4b4f6720f824a8a3da3d02b4f8a6628c958
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2024-11-07 08:31:37 +0000

    Complete JavaScript calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let ans = array.reduce((total, num) => {
+    return total + num;
+  }, 0);
+
+  return ans;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let ans = array.reduce((total, num) => {
+    return total * num;
+  }, 1);
 
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  let ans = 1;
+  for(let i = 0; i < pow; i++) {
+    ans *= num;
+  }
+
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let ans = 1;
+  for(let i = num; i > 0; i--) {
+    ans *= i;
+  }
+
+  return ans;
 };
 
 // Do not edit below this line
