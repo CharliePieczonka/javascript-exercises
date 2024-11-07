74878a59db7dfe1b3cdfc509ea22051395b8268c
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2024-11-07 08:58:09 +0000

    Complete JavaScript palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replaceAll("!" , "").replaceAll(".", "").replaceAll(",", "").toLowerCase().split(' ').join('');
 
+    do {
+        firstChar = string.slice(0, 1);
+        secondChar = string.slice(-1);
+        string = string.slice(1, -1);
+    } while(firstChar === secondChar && string.length > 1)
+
+    if(string.length <= 1) {
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
