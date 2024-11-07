4c1ae4fa30bf02988f2436e95a7e569ebb4de481
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2024-11-07 09:31:34 +0000

    Complete JavaScript fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(digit) {
+    if(digit < 0) {
+        return "OOPS";
+    }
 
+    if (digit == 0) {
+        return 0;
+    }
+    
+    let previous = 0;
+    let current = 1;
+    for(let i = 1; i < digit; i++) {
+        let newCurr = previous + current;
+        previous = current;
+        current = newCurr;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
