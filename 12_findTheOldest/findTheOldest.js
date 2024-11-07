623b23c844d7850aedaad56d65b0cf6020a877a9
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2024-11-07 09:52:40 +0000

    Complete JavaScript findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrOfObj) {
+    const currentYear = (new Date()).getFullYear();
 
+    let sortOldest = arrOfObj.sort((a, b) =>{
+        aAge = 0;
+        bAge = 0;
+        
+        if(!("yearOfDeath" in a)) {
+            aAge = currentYear - a.yearOfBirth;
+        }
+        else {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+
+        if(!("yearOfDeath" in b)) {
+            bAge = currentYear - b.yearOfBirth;
+        }
+        else {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+
+        return aAge < bAge ? 1 : -1;
+    });
+
+    return sortOldest[0];
 };
 
 // Do not edit below this line
